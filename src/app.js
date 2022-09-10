add4a0038d56ac5b665dbc0147dae1398fb755f3
OlenkaZh <elena.deptak@gmail.com>
2022-09-10 21:20:11 +0300

    added project

---
+++ src/app.js
@@ -0,0 +1,111 @@
+let now = new Date();
+let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thuesday", "Friday", "Saturday"];
+let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+  ];
+
+  let tempCelsium;
+  let tempFahrenheit;
+
+  let apiKey = "a98cf904e4273ceac14ee7e5c042eda8";
+
+  function formatDate() {
+    let day = days[now.getDay()];
+    let month = months[now.getMonth()];
+    let number = now.getDate();
+    let hour = now.getHours();
+    let minutes = now.getMinutes();
+
+    if(minutes < 10) {
+      minutes = `0${minutes}`;
+    }
+
+    let currentDate = `${day}, ${month} ${number}, ${hour}:${minutes}`;
+    let date = document.querySelector("#currentDate");
+    date.innerHTML = currentDate;
+  }
+
+  formatDate();
+
+  function displayCity(event) {
+    event.preventDefault()
+    let cityInput = document.querySelector("#cityInput").value;
+    let city = document.querySelector("#city");
+    let result = cityInput.charAt(0).toUpperCase() + cityInput.slice(1);
+
+    getWeatherByCity(result);
+
+    city.innerHTML = result;
+  }
+
+  let searching = document.querySelector("#addon-wrapping");
+  searching.addEventListener("click", displayCity);
+  document.addEventListener('keypress', (event)=>{
+    let keyCode = event.keyCode ? event.keyCode : event.which;
+
+    if(keyCode === 13) {
+      searching.click();
+    }
+  });
+
+  let myLocation = document.querySelector("#location");
+  myLocation.addEventListener("click", getLocationInfo);
+
+  function getLocationInfo() {
+    navigator.geolocation.getCurrentPosition(getWeatherByGeolocation);
+  }
+
+  function convertToFahrenheit(event) {
+    event.preventDefault()
+    tempFahrenheit = Math.round((tempCelsium*1.8)+32);
+    let temp = document.querySelector("#temperature");
+    temp.innerHTML = tempFahrenheit;
+  }
+
+  let convertingToFar = document.querySelector("#fahrenheit");
+  convertingToFar.addEventListener("click", convertToFahrenheit);
+
+  function convertToCelsius(tempCelsium) {
+    event.preventDefault()
+    let temp = document.querySelector("#temperature");
+    temp.innerHTML = tempCelsium;
+  }
+
+  let convertingToCel = document.querySelector("#celsius");
+  convertingToCel.addEventListener("click", convertToCelsius);
+
+  function showWeatherInfo(response) {
+    console.log(response.data);
+    let cityInput = response.data.name;
+    let city = document.querySelector("#city");
+    let result = cityInput.charAt(0).toUpperCase() + cityInput.slice(1);
+    city.innerHTML = result;
+    tempCelsium = Math.round(response.data.main.temp);
+
+    convertToCelsius(tempCelsium);
+  }
+
+  function getWeatherByGeolocation(position) {
+    let lat = position.coords.latitude;
+    let lon = position.coords.longitude;
+    let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`;
+    axios.get(url).then(showWeatherInfo);
+  }
+
+  navigator.geolocation.getCurrentPosition(getWeatherByGeolocation);
+
+  function getWeatherByCity(city) {
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
+    axios.get(url).then(showWeatherInfo);
+  }
